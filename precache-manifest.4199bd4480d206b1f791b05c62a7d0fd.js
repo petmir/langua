791e1e3b44d0d13a4fd819f435e8fa153cc7a5e9
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-01 11:58:40 +0000

    Deploy nai888/langua to github.com/nai888/langua.git:gh-pages

---
+++ precache-manifest.4199bd4480d206b1f791b05c62a7d0fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "832dff95750b51879323",
+    "url": "/langua/static/js/main.832dff95.chunk.js"
+  },
+  {
+    "revision": "bcfa700462581013f7ea",
+    "url": "/langua/static/css/1.631a534a.chunk.css"
+  },
+  {
+    "revision": "bcfa700462581013f7ea",
+    "url": "/langua/static/js/1.bcfa7004.chunk.js"
+  },
+  {
+    "revision": "8c5ea08df8107dc25503",
+    "url": "/langua/static/js/runtime~main.8c5ea08d.js"
+  },
+  {
+    "revision": "67066d6875c919543a710b31809798f5",
+    "url": "/langua/index.html"
+  }
+];
